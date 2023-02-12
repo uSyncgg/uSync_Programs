067c97d3caaaf91ec121ee9bba6d631c374356a8
Justin Owens <justin.owens3451@gmail.com>
2023-02-12 17:42:37 -0500

    Update homepage

--- usync-homepage/src/pages/Home.js
+++ usync-homepage/src/pages/Home.js
@@ -1,13 +1,14 @@
 import "../App.css";
 
-const computerPic = new URL("../images/computer.png", import.meta.url)
 const usyncLogo = new URL("../images/Usync-logo.png", import.meta.url)
 
 const Home = () => {
     return (
         <section className="home-top">
-            <img src={usyncLogo} alt="Usync-logo" id="usyncLogo"/>
-            <h1>Finding esports competitions is challenging. We make finding competition for each game not only simple, but all in one place.</h1>
+            <img src={usyncLogo} alt="Usync-logo" id="home-usyncLogo"/>
+            <h1 id="finding-esports">Finding esports competitions is challenging. We make finding competition for each game not only simple, but all in one place.</h1>
+            <button className="btn btn-primary" type="button" id="get-started">Get Started</button>
+            <h1 id="we-sync">We Sync So uSync</h1>
         </section>
     );
 }
