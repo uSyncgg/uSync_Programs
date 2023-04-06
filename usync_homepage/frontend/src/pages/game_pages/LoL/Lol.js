11587c90f8cf7c7ac9a005c150a1a0fe9ca0da49
JakeBrewskie <jakebruscino@gmail.com>
2023-04-06 15:00:02 -0400

    Fixed pathings for the gamepages that were incorrect

--- usync_homepage/frontend/src/pages/game_pages/LoL/Lol.js
+++ usync_homepage/frontend/src/pages/game_pages/LoL/Lol.js
@@ -17,15 +17,15 @@ const Lol = () => {
                 <div className='container'>
                     <div className='row'>
                         <div className='col-2 offset-3'>
-                            <a href='http://localhost:3000/games/call-of-duty/head-to-head'>
+                            <a href='http://localhost:3000/games/LoL/head-to-head'>
                             <img src="https://i.imgur.com/CiO5ViA.png" alt="Cod Head to Head" className='blockimg1' /> </a>
-                            <a href='http://localhost:3000/games/call-of-duty/lans'>
+                            <a href='http://localhost:3000/games/LoL/lans'>
                             <img src="https://i.imgur.com/NXTgpHd.png" alt="Cod Lans" className='blockimg2' /></a>
-                            <a href='http://localhost:3000/games/call-of-duty/leagues'>
+                            <a href='http://localhost:3000/games/LoL/leagues'>
                             <img src="https://i.imgur.com/Zj3dVpv.png" alt="Cod Leagues" className='blockimg3' /></a>
-                            <a href='http://localhost:3000/games/call-of-duty/NEED PATHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH'>
+                            <a href='http://localhost:3000/games/LoL/NEED PATHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH'>
                             <img src="https://i.imgur.com/nVx3C4M.png" alt="Cod Tourneys" className='blockimg4' /></a>
-                            <a href='http://localhost:3000/games/call-of-duty/wagers'>
+                            <a href='http://localhost:3000/games/LoL/wagers'>
                             <img src="https://i.imgur.com/EgBRBVS.png" alt="Cod Wagers" className='blockimg5' /></a>
                         </div>
                     </div>
