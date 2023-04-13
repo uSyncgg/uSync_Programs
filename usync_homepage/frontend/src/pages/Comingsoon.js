72871f338be90d447a5c340252bec0e26b783587
JakeBrewskie <jakebruscino@gmail.com>
2023-04-13 14:47:33 -0400

    Created the coming soon page and edited the scroll bar

--- usync_homepage/frontend/src/pages/Comingsoon.js
+++ usync_homepage/frontend/src/pages/Comingsoon.js
@@ -1,69 +1,55 @@
+import '../Info.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import Footer from '../Footer';
+import styles from '../Home.module.css';
 
 const Comingsoon = () => {
     return (
-        <div className="container">
-            <div className="coming-soon-header">
-                <h1>Coming  Soon</h1>
-                <img src="{shortBar}" alt="short-bar" />
-            </div>
-            <div className="coming-soon-game-titles">
-                <h1>Game Titles</h1>
-                <img src="{shortBar}" alt="short-bar" />
-                <br />
-                <br />
-                <div className="coming-soon-game-img">
-                    <img src="{youtube}" alt="APEX" />
-                    <img src="{youtube}" alt="Smash" />
-                    <img src="{youtube}" alt="Overwatch" />
-                </div>
-                <br />
-                <p>
-                    We are actively working on different titles in order to bring the best competition to you. These 
-                    take time due to the competitive scenes varying from title to title. Your continued use of our 
-                    services allow for us to keep working on new titles for the future. Thank you!
-                </p>
-                <br />
-                <br />
-            </div>
-            <div className="coming-soon-event-types">
-                <h1>Event Types</h1>
-                <img src="{shortBar}" alt="short-bar" />
-                <br />
-                <img src="{youtube}" alt="online tournaments" id='coming-soon-online-tournaments'/>
-                <br />
-                <br />
-                <p>
-                    We are looking to provide a list of daily tournaments from all of the varying sites so that players don't
-                    have to move from site to site to find what they are looking for!
-                </p>
-                <br />
-                <p>
-                    Unfortunately, implementing this feature is costly and our services are free which places a heavy burden on us.
-                    However, in the meantime keep up to date with what we are working on with our Twitter!
-                </p>
-                <br />
-                <br />
-                <blockquote class="twitter-tweet"><p lang="en" dir="ltr">Want to join the ultimate Esports experience? <a href="https://t.co/3nSDIXiWJq">pic.twitter.com/3nSDIXiWJq</a></p>&mdash; uSync (@uSyncGG) <a href="https://twitter.com/uSyncGG/status/1610732508508815360?ref_src=twsrc%5Etfw">January 4, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                <br />
-                <br />
-                <hr />
-                <br />
-            </div>
-            <div className="coming-soon-support-us">
-                <h1>Support Us</h1>
-                <img src="{shortBar}" alt="short-bar" />
-                <br />
-                <br />
-                <img src="{gofundme}" alt="go fund me" id="coming-soon-gfm"/>
-                <p>Support Our Vision</p>
-                <br />
-                <br />
-                <br />
-            </div>
-            <Footer/>
-        </div>
-    )
-}
+        <div>
+            <body id='league-body2'>
+                <header>
+                        <img src="https://i.imgur.com/0Wrn8wp.png" className='Header-Img' />
+                </header>
+                <div className='Background-mid-contact'>
+                    <div>
+                    <h1 className='event-title-contactus'>Game Titles</h1>
+                        <img src="https://i.imgur.com/eNhKhTI.png" className='Underline-h-verification-coming' />
+                    </div>
+                    <div className={styles.backgroundImg}>
+                    <a href='http://localhost:3000/Comingsoon'>
+                        <img src="https://i.imgur.com/MUcaqmt.png" alt="Apex Legends"/> </a>
 
+                    <a href='http://localhost:3000/Comingsoon'>
+                        <img src="https://i.imgur.com/qpQDtRJ.png" alt="Super Smash Brothers"/></a>
+
+                    <a href='http://localhost:3000/Comingsoon'>
+                        <img src="https://i.imgur.com/XxOYZiE.png" alt="Overwatch 2"/></a>
+                </div>
+
+                    <div className='container'>
+                        <div className='row' id='verif-row'>
+                            <div className='col-8 offset-2'>
+                                <h1 className='coming-soon-text'>We are actively working on different titles in order to bring the best competition to you. These take time due to the 
+                                competitive scenes varying from title to title. Your continued use of our services allow for us to keep working on new titles for the future. Thank you!</h1>
+                                <h1 className='event-title-coming'>Event Types</h1>
+                                <h1 className='info-title-coming2'>We are looking to provide a list of daily tournaments from all of the varying sites so that players don't have to move 
+                                from site to site to find what they are looking for. Eventually, we will have EVERY tournament in the world posted on uSync, just give us some time!</h1>
+                        <img src="https://i.imgur.com/eNhKhTI.png" className='Underline-h-coming' />
+                            </div>
+                            <div className='col-2' id='bubble-coming'>
+                                <img src="https://i.imgur.com/8ZFHAM6.png" alt="RocketLeague Tournaments" className='blockimg' />
+                                
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+            </body>
+            <Footer />
+        </div>
+
+    );
+}
 export default Comingsoon;
+
