dc39f578bc729da5ff59a34aace65fc95d44eb5b
Justin Owens <justin.owens3451@gmail.com>
2023-05-06 19:10:10 -0400

    Add Tabs to the navbar

--- usync_homepage/frontend/src/Playground.js
+++ usync_homepage/frontend/src/Playground.js
@@ -19,7 +19,7 @@ function Playground() {
           <li className={`nav-item ${location === '/' ? 'active' : ''}`}>
             <a className="nav-link" href="/">Home</a>
           </li>
-          <li className={`nav-item dropdown ${location.startsWith('/games') ? 'active' : ''}`}>
+          <li className={`nav-item dropdown ${(location.startsWith('/games') && !location.includes('/leagues') && !location.includes('/Tournaments') && !location.includes('/lans'))? 'active' : ''}`}>
             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Games
             </a>
@@ -30,6 +30,45 @@ function Playground() {
               <a className={`dropdown-item ${location === '/games/halo' ? 'active' : ''}`} href="/games/halo">Halo</a>
               <a className={`dropdown-item ${location === '/games/RocketLeague' ? 'active' : ''}`} href="/games/RocketLeague">Rocket League</a>
               <a className={`dropdown-item ${location === '/games/Valorant' ? 'active' : ''}`} href="/games/Valorant">Valorant</a>
+            </div>
+          </li>
+          <li className={`nav-item dropdown ${location.includes('/Tournaments') ? 'active' : ''}`}>
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Tournaments
+            </a>
+            <div className="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
+              <a className={`dropdown-item ${location === '/CoD/Tournaments' ? 'active' : ''}`} href="/CoD/Tournaments">Call of Duty</a>
+              {/*<a className={`dropdown-item ${location === '/games/warzone' ? 'active' : ''}`} href="/games/warzone">Warzone 2.0</a>
+              <a className={`dropdown-item ${location === '/games/LoL' ? 'active' : ''}`} href="/games/LoL">League of Legends</a>
+              <a className={`dropdown-item ${location === '/games/halo' ? 'active' : ''}`} href="/games/halo">Halo</a>
+              <a className={`dropdown-item ${location === '/games/RocketLeague' ? 'active' : ''}`} href="/games/RocketLeague">Rocket League</a>
+  <a className={`dropdown-item ${location === '/games/Valorant' ? 'active' : ''}`} href="/games/Valorant">Valorant</a> */}
+            </div>
+          </li>
+          <li className={`nav-item dropdown ${location.includes('/leagues') ? 'active' : ''}`}>
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Leagues
+            </a>
+            <div className="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
+              <a className={`dropdown-item ${location === '/games/call-of-duty/leagues' ? 'active' : ''}`} href="/games/call-of-duty/leagues">Call of Duty</a>
+              <a className={`dropdown-item ${location === '/games/warzone/leagues' ? 'active' : ''}`} href="/games/warzone/leagues">Warzone 2.0</a>
+              <a className={`dropdown-item ${location === '/games/LoL/leagues' ? 'active' : ''}`} href="/games/LoL/leagues">League of Legends</a>
+              <a className={`dropdown-item ${location === '/games/halo/leagues' ? 'active' : ''}`} href="/games/halo/leagues">Halo</a>
+              <a className={`dropdown-item ${location === '/games/RocketLeague/leagues' ? 'active' : ''}`} href="/games/RocketLeague/leagues">Rocket League</a>
+              <a className={`dropdown-item ${location === '/games/Valorant/leagues' ? 'active' : ''}`} href="/games/Valorant/leagues">Valorant</a>
+            </div>
+          </li>
+          <li className={`nav-item dropdown ${location.includes('/lans') ? 'active' : ''}`}>
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              LANs
+            </a>
+            <div className="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
+              <a className={`dropdown-item ${location === '/games/call-of-duty/lans' ? 'active' : ''}`} href="/games/call-of-duty/lans">Call of Duty</a>
+              <a className={`dropdown-item ${location === '/games/warzone/lans' ? 'active' : ''}`} href="/games/warzone/lans">Warzone 2.0</a>
+              <a className={`dropdown-item ${location === '/games/LoL/lans' ? 'active' : ''}`} href="/games/LoL/lans">League of Legends</a>
+              <a className={`dropdown-item ${location === '/games/halo/lans' ? 'active' : ''}`} href="/games/halo/lans">Halo</a>
+              {/*<a className={`dropdown-item ${location === '/games/RocketLeague/lans' ? 'active' : ''}`} href="/games/RocketLeague/lans">Rocket League</a>
+              <a className={`dropdown-item ${location === '/games/Valorant/lans' ? 'active' : ''}`} href="/games/Valorant/lans">Valorant</a>*/}
             </div>
           </li>
           <li className={`nav-item ${location === '/Comingsoon' ? 'active' : ''}`}>
