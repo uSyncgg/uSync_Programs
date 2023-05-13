571f00ef397286a74fdab679ee5a5f89ae9ebd72
JakeBrewskie <jakebruscino@gmail.com>
2023-05-13 12:04:14 -0400

    Added new cod league, updated game page bubbles for coming soon

--- usync_homepage/frontend/src/pages/game_pages/CoD/Cod_Leagues.js
+++ usync_homepage/frontend/src/pages/game_pages/CoD/Cod_Leagues.js
@@ -281,7 +281,23 @@ const Cod_Leagues = () => {
                                 </a>
                             </div>
                         </div>
-                        
+                        <p className='hr' />
+
+                        <div className='col-lg-6'>
+                            <div className='league'>
+                                <a href='http://localhost:3000/games/call-of-duty/leagues/sin-city-league'>
+                                    <h1 className='Leagueh1'>Sin City Co-Ed League</h1>
+                                </a>
+                                <div className='parent'>
+                                    <a href="http://localhost:3000/games/call-of-duty/leagues/sin-city-league"><img src="https://i.imgur.com/bGCOqXj.png" className='league-img' /></a>
+                                </div>
+                                <p className='Leaguep'>All Regions</p>
+                                {/* <img src={check} alt="uSync Verified" className='check' /> */}
+                                <a href='http://localhost:3000/games/call-of-duty/leagues/sin-city-league'>
+                                    <button type="submit" className='info-button'>More Info</button>
+                                </a>
+                            </div>
+                        </div>
 
                         <div>
                         <p className='hr' />
