2d98f9e9a4796dcdc7c5cc59c7c2a546d9cfd73f
Justin Owens <justin.owens3451@gmail.com>
2023-06-02 09:19:28 -0500

    Update scrolling on forms

--- usync_homepage/frontend/src/pages/Home.js
+++ usync_homepage/frontend/src/pages/Home.js
@@ -80,57 +80,57 @@ const Home = () => {
 
             <div className={styles.hr}></div>
 
-          <h2 className={styles.hometitles}>LAN Events</h2>
-          <img
-            className="underline-h"
-            src="https://i.imgur.com/eNhKhTI.png"
-            alt="underline"
-          />
-          <iframe
-            className={styles.Map}
-            src="https://www.google.com/maps/d/u/6/embed?mid=1QrCfeXFSyZJMXLzUeAiIWy0hcWgaJVg&ehbc=2E312F"
-            width="640"
-            height="480"
-          ></iframe>
-          <h3 className={styles.learnhow}>
-            Learn how to post your LAN to the map{" "}
-            <a href="/more/eventhost" className={styles.eventhost}>
-              here
-            </a>
-          </h3>
-          <br />
-          <div className={styles.hr}></div>
+            <h2 className={styles.hometitles}>LAN Events</h2>
+            <img
+              className="underline-h"
+              src="https://i.imgur.com/eNhKhTI.png"
+              alt="underline"
+            />
+            <iframe
+              className={styles.Map}
+              src="https://www.google.com/maps/d/u/6/embed?mid=1QrCfeXFSyZJMXLzUeAiIWy0hcWgaJVg&ehbc=2E312F"
+              width="640"
+              height="480"
+            ></iframe>
+            <h3 className={styles.learnhow}>
+              Learn how to post your LAN to the map{" "}
+              <a href="/more/eventhost" className={styles.eventhost}>
+                here
+              </a>
+            </h3>
+            <br />
+            <div className={styles.hr}></div>
 
-          <h2 className={styles.hometitles}>Coming Soon!</h2>
-          <img
-            className="underline-h"
-            src="https://i.imgur.com/eNhKhTI.png"
-            alt="underline"
-          />
+            <h2 className={styles.hometitles}>Coming Soon!</h2>
+            <img
+              className="underline-h"
+              src="https://i.imgur.com/eNhKhTI.png"
+              alt="underline"
+            />
 
-          <div className={styles.backgroundImages}>
-            <a href="http://localhost:3000/Comingsoon">
-              <img src="https://i.imgur.com/MUcaqmt.png" alt="Apex Legends" />{" "}
-            </a>
+            <div className={styles.backgroundImages}>
+              <a href="http://localhost:3000/Comingsoon">
+                <img src="https://i.imgur.com/MUcaqmt.png" alt="Apex Legends" />{" "}
+              </a>
 
-            <a href="http://localhost:3000/Comingsoon">
-              <img
-                src="https://i.imgur.com/qpQDtRJ.png"
-                alt="Super Smash Brothers"
-              />
-            </a>
+              <a href="http://localhost:3000/Comingsoon">
+                <img
+                  src="https://i.imgur.com/qpQDtRJ.png"
+                  alt="Super Smash Brothers"
+                />
+              </a>
 
-            <a href="http://localhost:3000/Comingsoon">
-              <img src="https://i.imgur.com/XxOYZiE.png" alt="Overwatch 2" />
-            </a>
+              <a href="http://localhost:3000/Comingsoon">
+                <img src="https://i.imgur.com/XxOYZiE.png" alt="Overwatch 2" />
+              </a>
+            </div>
+            <br />
+            <div className={styles.hr}></div>
+            <br />
+            <br />
+            <br />
+            <br />
           </div>
-          <br />
-          <div className={styles.hr}></div>
-          <br />
-          <br />
-          <br />
-          <br />
-        </div>
         </div>
         <Footer />
       </div>
