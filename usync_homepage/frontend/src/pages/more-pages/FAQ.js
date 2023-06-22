5929c521a6992146a9d8bf14c471d2d1de6459c2
Justin Owens <justin.owens3451@gmail.com>
2023-06-21 20:59:01 -0500

    Update FAQ page, needs images

--- usync_homepage/frontend/src/pages/more-pages/FAQ.js
+++ usync_homepage/frontend/src/pages/more-pages/FAQ.js
@@ -8,25 +8,32 @@ const FAQ = () => {
             <div className="row FAQ-container">
                 <div className="col-sm-4">
                     <h4 className='FAQ-header-text'>Articles</h4>
+                    <div className="container-border">
                     <div className="images-container">
                        <img className='FAQ-img' src="https://i.imgur.com/5riYNow.png" alt="img" />
                        <img className='FAQ-img' src="https://i.imgur.com/5riYNow.png" alt="img" />
                        <img className='FAQ-img' src="https://i.imgur.com/5riYNow.png" alt="img" />
                     </div>
-
+                    </div>
                     <h4 className="FAQ-header-text">Games</h4>
+                    <div className="container-border">
                     <div className="images-container">
                        <img className='FAQ-img' src="https://i.imgur.com/5riYNow.png" alt="img" />
                        <img className='FAQ-img' src="https://i.imgur.com/5riYNow.png" alt="img" />
                        <img className='FAQ-img' src="https://i.imgur.com/5riYNow.png" alt="img" />
                     </div>
+                    </div>
+                    
 
                     <h4 className="FAQ-header-text">Socials</h4>
-                    <div className="socials-images-container">
+                    <div className="container-border">
+                    <div className="images-container">
                        <img className='FAQ-img' src="https://i.imgur.com/5riYNow.png" alt="img" />
                        <img className='FAQ-img' src="https://i.imgur.com/5riYNow.png" alt="img" />
                        <img className='FAQ-img' src="https://i.imgur.com/5riYNow.png" alt="img" />
                     </div>
+                    </div>
+                    
                 </div>
                 <div className="col-sm-8">
                     <h4 className="FAQ-header-text">FAQ</h4>
@@ -37,29 +44,31 @@ const FAQ = () => {
                             Please feel free to contact us.</h6>
                     </div>
                     <div className="FAQ-text-container">
-                        <h6 className="FAQ-q">Q: </h6>
-                        <h6 className="FAQ-a">A: </h6>
+                        <h6 className="FAQ-q">Q: How to Start?</h6>
+                        <h6 className="FAQ-a">A: Once you know what game you want to participate in, click 'Join Now', Register, and sign up. Please feel free to contact us.</h6>
                     </div>
                     <div className="FAQ-text-container">
-                        <h6 className="FAQ-q">Q: </h6>
-                        <h6 className="FAQ-a">A: </h6>
+                        <h6 className="FAQ-q">Q: What's the easiest way to navigate the site?</h6>
+                        <h6 className="FAQ-a">A: The best way to navigate the site is either going page-to-page or watching this short video. <a className='FAQ-link' href="/">Link</a></h6>
                     </div>
                     <div className="FAQ-text-container">
-                        <h6 className="FAQ-q">Q: </h6>
-                        <h6 className="FAQ-a">A: </h6>
+                        <h6 className="FAQ-q">Q: Best way to learn the rules?</h6>
+                        <h6 className="FAQ-a">A: Luckily, we include game rules on our site. <a className='FAQ-link' href="/">Link</a></h6>
                     </div>
                     <div className="FAQ-text-container">
-                        <h6 className="FAQ-q">Q: </h6>
-                        <h6 className="FAQ-a">A: </h6>
+                        <h6 className="FAQ-q">Q: How to enter a league?</h6>
+                        <h6 className="FAQ-a">A: The league you wish to join will explain everything on the host site. We also explain it in short-terms on the select league page.</h6>
                     </div>
                     <div className="FAQ-text-container">
-                        <h6 className="FAQ-q">Q: </h6>
-                        <h6 className="FAQ-a">A: </h6>
+                        <h6 className="FAQ-q">Q: What is uSync Verified?</h6>
+                        <h6 className="FAQ-a">A: uSync verified is a verification feature for hosts to lead the others as a form of status that allows a safe environment for players. <a className='FAQ-link' href="/">learn more</a></h6>
                     </div>
+                    <button className='FAQ-button'>Join The Fun ></button>
                 </div>
             </div>
         </div>
         </div>
+        
     </div>
   )
 }
