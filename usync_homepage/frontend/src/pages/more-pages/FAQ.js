52d21e9cc21cab4f2fdfd025894b5e28a51c9971
JakeBrewskie <jakebruscino@gmail.com>
2023-07-08 14:22:25 -0400

    Fixed FAQ page with updates

--- usync_homepage/frontend/src/pages/more-pages/FAQ.js
+++ usync_homepage/frontend/src/pages/more-pages/FAQ.js
@@ -1,4 +1,5 @@
 import './FAQ.css'
+import Footer from '../../Footer';
 
 const FAQ = () => {
   return (
@@ -7,14 +8,6 @@ const FAQ = () => {
         <div className="container-fluid">
             <div className="row FAQ-container">
                 <div className="col-sm-4">
-                    <h4 className='FAQ-header-text'>Articles</h4>
-                    <div className="container-border">
-                        <div className="images-container">
-                            <img className='FAQ-img' src="https://i.imgur.com/5riYNow.png" alt="img" />
-                            <img className='FAQ-img' src="https://i.imgur.com/5riYNow.png" alt="img" />
-                            <img className='FAQ-img' src="https://i.imgur.com/5riYNow.png" alt="img" />
-                        </div>
-                    </div>
                     <h4 className="FAQ-header-text">Games</h4>
                     <div className="container-border">
                         <div className="images-container">
@@ -43,37 +36,44 @@ const FAQ = () => {
                 <div className="col-sm-8">
                     <h4 className="FAQ-header-text">FAQ</h4>
                     <div className="FAQ-text-container-top">
-                        <h6 className="FAQ-q">Q: How to sign up?</h6>
-                        <h6 className="FAQ-a">A: You can sign up directly through the links provided. 
-                            You will be redirected to the given host where you can register. 
-                            Please feel free to contact us.</h6>
+                        <h6 className="FAQ-q">Q: How do I sign up for competitions?</h6>
+                        <h6 className="FAQ-a">A: You can sign up directly from the "Join Now" button on your desired event. 
+                            From there, you will be redirected to that event's registeration page. 
+                            If you have any trouble, feel free to <a className='FAQ-link' href="http://localhost:3000/more/contactus" target="_blank">Contact Us</a></h6>
                     </div>
                     <div className="FAQ-text-container">
-                        <h6 className="FAQ-q">Q: How to Start?</h6>
-                        <h6 className="FAQ-a">A: Once you know what game you want to participate in, click 'Join Now', Register, and sign up. Please feel free to contact us.</h6>
+                        <h6 className="FAQ-q">Q: What are the different types of competitions?</h6>
+                        <h6 className="FAQ-a">A: There are 5 types of competitions for each game: Tournaments, Wagers, LANs, Head-to-head, and Leagues. To learn more about each type, check out our video explaining them <a className='FAQ-link' href="/" target="_blank">Here</a></h6>
                     </div>
                     <div className="FAQ-text-container">
                         <h6 className="FAQ-q">Q: What's the easiest way to navigate the site?</h6>
-                        <h6 className="FAQ-a">A: The best way to navigate the site is either going page-to-page or watching this short video. <a className='FAQ-link' href="/">Link</a></h6>
+                        <h6 className="FAQ-a">A: The best way to navigate the site is either going page-to-page or watching this <a className='FAQ-link' href="https://www.youtube.com/watch?v=ojWSFWJX4b8" target="_blank">Short video</a></h6>
                     </div>
                     <div className="FAQ-text-container">
-                        <h6 className="FAQ-q">Q: Best way to learn the rules?</h6>
-                        <h6 className="FAQ-a">A: Luckily, we include game rules on our site. <a className='FAQ-link' href="/">Link</a></h6>
+                        <h6 className="FAQ-q">Q: What are the rules?</h6>
+                        <h6 className="FAQ-a">A: Each game title has a different ruleset based on the gamemode. The best way to find the rules is to check through the provider you are playing for as each has their own variation. Here is an <a className='FAQ-link' href="https://www.checkmategaming.com/ladder/cross-platform/call-of-duty-modern-warfare-ii/rules" target="_blank">Example</a></h6>
                     </div>
                     <div className="FAQ-text-container">
-                        <h6 className="FAQ-q">Q: How to enter a league?</h6>
-                        <h6 className="FAQ-a">A: The league you wish to join will explain everything on the host site. We also explain it in short-terms on the select league page.</h6>
+                        <h6 className="FAQ-q">Q: How do I post my own event?</h6>
+                        <h6 className="FAQ-a">A: You can post your own event by submitting an <a className='FAQ-link' href="http://localhost:3000/more/eventhost" target="_blank">Event Host</a> form. Once submitted we will ensure the event is to the uSync standard, and post it within 2-3 business days.</h6>
                     </div>
                     <div className="FAQ-text-container">
                         <h6 className="FAQ-q">Q: What is uSync Verified?</h6>
-                        <h6 className="FAQ-a">A: uSync verified is a verification feature for hosts to lead the others as a form of status that allows a safe environment for players. <a className='FAQ-link' href="/">learn more</a></h6>
+                        <h6 className="FAQ-a">A: uSync verified is our way of ensuring the legitimacy of our event hosts. All events with the uSync Verified checkmark have been independently verified by our team to be a safe, and quality competition for all entrants. <a className='FAQ-link' href="http://localhost:3000/more/verification">Learn More</a></h6>
                     </div>
-                    <button className='FAQ-button'>Join The Fun ></button>
+                    <h4 className='FAQ-header-text-article'>Articles</h4>
+                    <div className="container-border">
+                        <div className="images-container">
+                            <img className='FAQ-img' src="https://i.imgur.com/alnQaRb.png" alt="img" />
+                            <img className='FAQ-img' src="https://i.imgur.com/SoG2YYm.png" alt="img" />
+                            <img className='FAQ-img' src="https://i.imgur.com/bS208ZD.png" alt="img" />
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
         </div>
-        
     </div>
   )
 }
